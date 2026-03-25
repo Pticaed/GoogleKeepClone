6685253e18147fef101f6968cb551b61e507570e
NotVerySmart123221 <sashatop4ik23@gmail.com>
2026-03-26 00:10:48 +0800

    feat: update package dependencies and refactor database client initialization

--- src/db/client.tsx
+++ src/db/client.tsx
@@ -1,6 +1,15 @@
 import { drizzle } from "drizzle-orm/expo-sqlite";
 import { openDatabaseSync } from "expo-sqlite";
+import { Platform } from "react-native";
 import * as schema from "./schema";
 
-export const expoDb = openDatabaseSync("db.db");
-export const db = drizzle(expoDb, { schema });
+const createDb = () => {
+    if (Platform.OS === "web") {
+        return { db: null as any, expoDb: null as any };
+    }
+    const expoDb = openDatabaseSync("db.db");
+    const db = drizzle(expoDb, { schema });
+    return { db, expoDb };
+};
+
+export const { db, expoDb } = createDb();
